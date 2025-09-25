6597b7b4464ec3765fe4bd6ca85181c676276ec9
Mohammed Debashi <mohammed.debashi@defra.gov.uk>
2025-09-25 02:09:32 +0100

    debugging with logs

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,28 +1,38 @@
 /* istanbul ignore file */
 
 import { config } from '~/src/config/config.js'
+import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 
 export const homeController = {
   handler: async (request, h) => {
     try {
+      const logger = createLogger()
       request.yar.clear('basic-upload')
       const endpointUrl = config.get('cdpUploader.baseUrl') + '/initiate'
       const bucket = config.get('bucket')
 
-      request.server.log(['debug', 'upload'], {
-        endpointUrl,
-        bucket
-      })
+      const requestPayload = {
+        redirect: '/upload/complete',
+        s3Bucket: bucket
+      }
 
-      const response = await fetch(endpointUrl, {
+      const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          redirect: '/upload/complete',
-          s3Bucket: config.get('bucket')
-        })
+        body: JSON.stringify(requestPayload)
+      }
+
+      // delete this log after debugging
+      logger.info(['env temporary'], {
+        endpointUrl,
+        requestOptions: {
+          ...requestOptions,
+          // log parsed body instead of raw JSON string
+          body: requestPayload
+        }
       })
 
+      const response = await fetch(endpointUrl, requestOptions)
       if (!response.ok) {
         const errorBody = await response.text()
         request.server.log(
