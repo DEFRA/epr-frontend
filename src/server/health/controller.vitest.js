398406e74a966522961388ffe5c10484593a8e18
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-09-30 17:50:41 +0100

    create vitest version of health/controller test

---
+++ src/server/health/controller.vitest.js
@@ -0,0 +1,31 @@
+import { describe, test, expect, beforeAll, afterAll } from 'vitest'
+import { createServer } from '~/src/server/index.js'
+import { statusCodes } from '~/src/server/common/constants/status-codes.js'
+
+describe('#healthController', () => {
+  /** @type {Server} */
+  let server
+
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    await server.stop({ timeout: 0 })
+  })
+
+  test('Should provide expected response', async () => {
+    const { result, statusCode } = await server.inject({
+      method: 'GET',
+      url: '/health'
+    })
+
+    expect(result).toEqual({ message: 'success' })
+    expect(statusCode).toBe(statusCodes.ok)
+  })
+})
+
+/**
+ * @import { Server } from '@hapi/hapi'
+ */
