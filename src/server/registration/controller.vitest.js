307e7f6726b323aa1e709914b4733e37213e4012
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-09-30 17:52:54 +0100

    create vitest version of registration/controller test

---
+++ src/server/registration/controller.vitest.js
@@ -0,0 +1,33 @@
+import { describe, test, expect, beforeAll, afterAll } from 'vitest'
+import { createServer } from '~/src/server/index.js'
+import { statusCodes } from '~/src/server/common/constants/status-codes.js'
+
+describe('#registrationController', () => {
+  const organisationId = '123'
+  const registrationId = '456'
+  /** @type {Server} */
+  let server
+
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    await server.stop({ timeout: 0 })
+  })
+
+  test('Should provide expected response', async () => {
+    const { result, statusCode } = await server.inject({
+      method: 'GET',
+      url: `/organisations/${organisationId}/registrations/${registrationId}`
+    })
+
+    expect(result).toEqual(expect.stringContaining('Registration |'))
+    expect(statusCode).toBe(statusCodes.ok)
+  })
+})
+
+/**
+ * @import { Server } from '@hapi/hapi'
+ */
