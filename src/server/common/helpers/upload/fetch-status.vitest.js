89fb15eb4f8af9c78abbf9e6a44b9beffeb59aef
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-09-30 17:43:15 +0100

    create vitest version of upload/fetch-status test

---
+++ src/server/common/helpers/upload/fetch-status.vitest.js
@@ -0,0 +1,22 @@
+import { describe, test, expect, vi } from 'vitest'
+import { fetchStatus } from './fetch-status.js'
+import fetch from 'node-fetch'
+
+vi.mock('node-fetch', () => ({
+  default: vi.fn().mockResolvedValue({
+    json: vi.fn()
+  })
+}))
+
+describe('fetchStatus', () => {
+  test('should call fetch', async () => {
+    const id = 'abc123'
+    await fetchStatus(id)
+
+    expect(fetch).toHaveBeenNthCalledWith(
+      1,
+      expect.stringMatching(new RegExp(`/status/${id}$`)),
+      expect.any(Object)
+    )
+  })
+})
