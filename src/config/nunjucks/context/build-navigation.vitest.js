bf368a2de1358b5d9b3e71e3e2631a316292e59c
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-09-30 17:38:32 +0100

    create vitest version of build-navigation test

---
+++ src/config/nunjucks/context/build-navigation.vitest.js
@@ -0,0 +1,37 @@
+import { describe, expect, test } from 'vitest'
+import { buildNavigation } from '~/src/config/nunjucks/context/build-navigation.js'
+
+/**
+ * @param {Partial<Request>} [options]
+ */
+function mockRequest(options) {
+  return { ...options }
+}
+
+describe('#buildNavigation', () => {
+  test('Should provide expected navigation details', () => {
+    expect(
+      buildNavigation(mockRequest({ path: '/non-existent-path' }))
+    ).toEqual([
+      {
+        active: false,
+        href: '/',
+        text: 'Your sites'
+      }
+    ])
+  })
+
+  test('Should provide expected highlighted navigation details', () => {
+    expect(buildNavigation(mockRequest({ path: '/' }))).toEqual([
+      {
+        active: true,
+        href: '/',
+        text: 'Your sites'
+      }
+    ])
+  })
+})
+
+/**
+ * @import { Request } from '@hapi/hapi'
+ */
