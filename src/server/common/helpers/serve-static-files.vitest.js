b07b14d478fe1e213178e656de9aa856e9fef256
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-09-30 17:58:48 +0100

    create vitest version of serve-static-files test

---
+++ src/server/common/helpers/serve-static-files.vitest.js
@@ -0,0 +1,35 @@
+import { describe, test, expect, beforeEach, afterEach } from 'vitest'
+import { statusCodes } from '~/src/server/common/constants/status-codes.js'
+import { startServer } from '~/src/server/common/helpers/start-server.js'
+
+describe('#serveStaticFiles', () => {
+  let server
+
+  describe('When secure context is disabled', () => {
+    beforeEach(async () => {
+      server = await startServer()
+    })
+
+    afterEach(async () => {
+      await server.stop({ timeout: 0 })
+    })
+
+    test('Should serve favicon as expected', async () => {
+      const { statusCode } = await server.inject({
+        method: 'GET',
+        url: '/favicon.ico'
+      })
+
+      expect(statusCode).toBe(statusCodes.noContent)
+    })
+
+    test('Should serve assets as expected', async () => {
+      const { statusCode } = await server.inject({
+        method: 'GET',
+        url: '/public/assets/images/govuk-crest.svg'
+      })
+
+      expect(statusCode).toBe(statusCodes.ok)
+    })
+  })
+})
