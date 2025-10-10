8537af19f10837c975e934cb26edf158671d8f93
Mohammed Debashi <mohammed.debashi@defra.gov.uk>
2025-10-10 10:10:24 +0100

    using object notation option

--- src/server/common/helpers/errors.js
+++ src/server/common/helpers/errors.js
@@ -7,15 +7,15 @@ function statusCodeMessage(statusCode, localize) {
   console.log('localize: ', localize)
   switch (statusCode) {
     case statusCodes.notFound:
-      return localize('errors').notFound
+      return localize('errors.notFound')
     case statusCodes.forbidden:
-      return localize('errors').forbidden
+      return localize('errors.forbidden')
     case statusCodes.unauthorized:
-      return localize('errors').unauthorized
+      return localize('errors.unauthorized')
     case statusCodes.badRequest:
-      return localize('errors').badRequest
+      return localize('errors.badRequest')
     default:
-      return localize('errors').generic
+      return localize('errors.generic')
   }
 }
 
