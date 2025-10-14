0ca2b51ce6b8da1b7e2cde720f0f57d39472962c
Mohammed Debashi <mohammed.debashi@defra.gov.uk>
2025-10-14 09:48:30 +0100

    adding interpolation and pluralisation

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -5,12 +5,21 @@
  */
 export const homeController = {
   handler(request, h) {
-    console.log('request#############: ', request.t);
+    console.log('request#############: ', request.t)
     const localize = request.t
+    const itemCount0 = 0
+    const itemCount1 = 1
+    const itemCount3 = 3
+    const messageZero = localize('home:item', { count: itemCount0 })
+    const messageOne = localize('home:item', { count: itemCount1 })
+    const messageThree = localize('home:item', { count: itemCount3 })
     return h.view('home/index', {
       pageTitle: localize('common:serviceName'),
       heading: localize('home:pageTitle'),
-      greeting: localize('home:greeting', { name: 'Mohammed' })
+      greeting: localize('home:greeting', { name: 'John Doe' }),
+      messageZero,
+      messageOne,
+      messageThree
     })
   }
 }
