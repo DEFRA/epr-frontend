da24da9f0d9000924be68960a42cce320206532e
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-10-23 13:06:29 +0100

    PAE-000: upgrade to vitest@4.0.1
    helping out dependabot who was struggling with this one :D

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,6 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true,
     environment: 'node',
     clearMocks: true,
     hookTimeout: 60000,
