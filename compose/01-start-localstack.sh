838ce472836f3ebc7a566dadffd6df2fbb3f7e90
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-10-29 11:00:17 +0000

    PAE-441: fix cdp-uploader startup in docker compose

--- compose/01-start-localstack.sh
+++ compose/01-start-localstack.sh
@@ -10,7 +10,7 @@ export AWS_SECRET_ACCESS_KEY=test
 echo "[INIT SCRIPT] Creating buckets" >&2
 
 aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine
-aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-summary-logs
+aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
 
 echo "[INIT SCRIPT] Creating queues" >&2
 
@@ -20,13 +20,5 @@ aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploa
 
 # test harness
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name mock-clamav
-aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration\
-    --bucket cdp-uploader-quarantine \
-    --notification-configuration '{
-                                      "QueueConfigurations": [
-                                         {
-                                           "QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav",
-                                           "Events": ["s3:ObjectCreated:*"]
-                                         }
-                                       ]
-	                                }'
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cdp-uploader-quarantine --notification-configuration '{"QueueConfigurations": [{"QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav","Events": ["s3:ObjectCreated:*"]}]}'
+aws --endpoint-url=http://localhost:4566 sqs create-queue --region $AWS_REGION --queue-name cdp-uploader-download-requests
