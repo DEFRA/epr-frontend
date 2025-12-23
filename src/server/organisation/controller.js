a547703174ce1b842e44c8fefcf13a7a669a0ce2
Mohammed Debashi <mohammed.debashi@digital.mod.uk>
2025-12-23 10:28:49 +0000

    sonar fixes

--- src/server/organisation/controller.js
+++ src/server/organisation/controller.js
@@ -10,7 +10,7 @@ import { err } from '#server/common/helpers/result.js'
  * @returns {Array} Array of sites with their materials
  */
 function organiseAccreditationsBySite(data, wasteProcessingType) {
-  const EXCLUDED_STATUSES = ['created', 'rejected']
+  const EXCLUDED_STATUSES = new Set(['created', 'rejected'])
 
   // Filter accreditations by waste processing type
   const filteredAccreditations = data.accreditations.filter(
@@ -33,8 +33,8 @@ function organiseAccreditationsBySite(data, wasteProcessingType) {
 
     // Skip if EITHER status is created or rejected
     if (
-      EXCLUDED_STATUSES.includes(registrationStatus.toLowerCase()) &&
-      EXCLUDED_STATUSES.includes(accreditationStatus.toLowerCase())
+      EXCLUDED_STATUSES.has(registrationStatus.toLowerCase()) &&
+      EXCLUDED_STATUSES.has(accreditationStatus.toLowerCase())
     ) {
       return
     }
