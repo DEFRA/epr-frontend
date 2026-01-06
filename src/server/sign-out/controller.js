95afac12f8c1624e3908905571e0aacdba70e042
Mohammed Debashi <mohammed.debashi@digital.mod.uk>
2026-01-06 02:26:38 +0000

    fixing 404 issues

--- src/server/sign-out/controller.js
+++ src/server/sign-out/controller.js
@@ -3,22 +3,10 @@
  * Displays confirmation page after user has been signed out
  * @satisfies {Partial<ServerRoute>}
  */
+
 const controller = {
-  options: {
-    auth: { mode: 'try' }
-  },
   handler(request, h) {
-    const { t: localise, localiseUrl } = request
-
-    // Check if user came from proper logout flow
-    // Flash message is set by logout controller before redirecting to Defra ID
-    const signedOut = request.yar.flash('signedOut')?.at(0)
-
-    if (!signedOut) {
-      // Direct navigation - redirect to home
-      const homeUrl = localiseUrl ? localiseUrl('/') : '/'
-      return h.redirect(homeUrl)
-    }
+    const { t: localise } = request
 
     return h.view('sign-out/index', {
       pageTitle: localise('sign-out:pageTitle')
