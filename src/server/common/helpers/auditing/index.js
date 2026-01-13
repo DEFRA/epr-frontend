b4ce4599476820cc25d4248547e8bf9b5b0b5769
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2026-01-13 17:53:11 +0000

    PAE-808 Add auditing and metrics to sign-out (#322)
    Adds integration test to verify new behaviour
    
    Also
    - adds dependency on @defra/cdp-auditing
    - updates provideAuthedUser pre-handler to return null instead of undefined when no user logged in - returning undefined causes the service to 500

---
+++ src/server/common/helpers/auditing/index.js
@@ -0,0 +1,27 @@
+import { audit } from '@defra/cdp-auditing'
+
+function auditSignIn(userId, userEmail) {
+  auditSSO('sign-in', userId, userEmail)
+}
+
+function auditSignOut(userId, userEmail) {
+  auditSSO('sign-out', userId, userEmail)
+}
+
+function auditSSO(action, userId, userEmail) {
+  const payload = {
+    event: {
+      category: 'access',
+      subCategory: 'sso',
+      action
+    },
+    context: {},
+    user: {
+      id: userId,
+      email: userEmail
+    }
+  }
+  audit(payload)
+}
+
+export { auditSignIn, auditSignOut }
