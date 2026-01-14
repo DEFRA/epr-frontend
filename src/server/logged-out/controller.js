e4b2573f2160594b6604a2912f2e86f3ea202a97
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-01-14 09:53:24 +0000

    simplify logged-out controller test

--- src/server/logged-out/controller.js
+++ src/server/logged-out/controller.js
@@ -1,5 +1,3 @@
-import { getUserSession } from '#server/auth/helpers/get-user-session.js'
-
 /**
  * Logged out confirmation controller
  * Displays confirmation page after user has been logged out
@@ -7,14 +5,13 @@ import { getUserSession } from '#server/auth/helpers/get-user-session.js'
  */
 
 const controller = {
-  async handler(request, h) {
-    const { t: localise } = request
-    const { value: userSession } = await getUserSession(request)
-
-    if (userSession) {
+  handler(request, h) {
+    if (request.auth.credentials) {
       // If user is not logged out, redirect to home page
       return h.redirect('/')
     }
+
+    const { t: localise } = request
 
     return h.view('logged-out/index', {
       pageTitle: localise('logged-out:pageTitle')
