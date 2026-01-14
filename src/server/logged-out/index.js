8ace78cfa03fc428137d786b082e2f3cbad1cbf0
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-01-14 12:12:00 +0000

    drop auth req for /logged-out

--- src/server/logged-out/index.js
+++ src/server/logged-out/index.js
@@ -15,7 +15,7 @@ const loggedOut = {
           method: 'GET',
           path: '/logged-out',
           options: {
-            auth: { mode: 'try' }
+            auth: false
           }
         }
       ])
