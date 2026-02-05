9fe680b94dca30f8f20eaedf5d2d88c5fe228cd3
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-05 15:09:43 +0000

    PAE-926: add fetch-json helper
    - to hopefully reduce confusion!

--- src/server/common/helpers/fetch-json-from-backend.js
+++ src/server/common/helpers/fetch-json-from-backend.js
@@ -1,8 +1,5 @@
-import Boom from '@hapi/boom'
-
-import { withTraceId } from '@defra/hapi-tracing'
 import { config } from '#config/config.js'
-import { getTracingHeaderName } from './request-tracing.js'
+import { fetchJson } from './fetch-json.js'
 
 /**
  * Fetch JSON from a given path in the backend service.
@@ -12,48 +9,7 @@ import { getTracingHeaderName } from './request-tracing.js'
  */
 export const fetchJsonFromBackend = async (path, options) => {
   const eprBackendUrl = config.get('eprBackendUrl')
-
-  const completeOptions = {
-    ...options,
-    headers: withTraceId(getTracingHeaderName(), {
-      ...options?.headers,
-      'Content-Type': 'application/json'
-    })
-  }
-
   const url = path.startsWith('http') ? path : `${eprBackendUrl}${path}`
 
-  try {
-    const response = await fetch(url, completeOptions)
-
-    if (!response.ok) {
-      let errorBody = null
-      if (response.headers.get('content-type')?.includes('application/json')) {
-        errorBody = await response.json()
-      }
-
-      const error = Boom.boomify(
-        new Error(
-          `Failed to fetch from backend at url: ${url}: ${response.status} ${response.statusText}`
-        ),
-        { statusCode: response.status }
-      )
-
-      if (errorBody) {
-        error.output.payload = errorBody
-      }
-
-      throw error
-    }
-
-    return await response.json()
-  } catch (error) {
-    if (error.isBoom) {
-      throw error
-    }
-
-    throw Boom.internal(
-      `Failed to fetch from backend at url: ${url}: ${error.message}`
-    )
-  }
+  return fetchJson(url, options)
 }
