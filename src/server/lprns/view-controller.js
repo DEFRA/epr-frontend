56e8f3eb1b6d53f882a845af9d057ec190793e24
David Lusby <loveboat@users.noreply.github.com>
2026-02-06 17:56:01 +0000

    PAE-1044: issuer / issued / issued-by fun with prns (#489)
    * fix inline error message
    
    * fix issuer data
    
    * add some types
    
    ---------
    
    Co-authored-by: David Lusby <1230979+loveboat@users.noreply.github.com>

--- src/server/lprns/view-controller.js
+++ src/server/lprns/view-controller.js
@@ -2,6 +2,7 @@ import Boom from '@hapi/boom'
 
 import { config } from '#config/config.js'
 import { fetchRegistrationAndAccreditation } from '#server/common/helpers/organisations/fetch-registration-and-accreditation.js'
+import { getNoteTypeDisplayNames } from '#server/common/helpers/prns/registration-helpers.js'
 import { fetchWasteBalances } from '#server/common/helpers/waste-balance/fetch-waste-balances.js'
 import { buildAccreditationRows } from './helpers/build-accreditation-rows.js'
 import {
@@ -10,7 +11,6 @@ import {
   buildStatusRow
 } from './helpers/build-prn-detail-rows.js'
 import { fetchPackagingRecyclingNote } from './helpers/fetch-packaging-recycling-note.js'
-import { getNoteTypeDisplayNames } from '#server/common/helpers/prns/registration-helpers.js'
 import { getLumpyDisplayMaterial } from './helpers/get-lumpy-display-material.js'
 import { getStatusConfig } from './helpers/get-status-config.js'
 import { updatePrnStatus } from './helpers/update-prn-status.js'
@@ -188,8 +188,8 @@ async function handleDraftView(
 
   const prnDetailRows = buildDraftPrnDetailRows({
     prnDraft,
-    organisationData,
-    localise
+    localise,
+    organisationData
   })
 
   const accreditationRows = buildAccreditationRows({
@@ -244,21 +244,20 @@ async function handleExistingView(
   { organisationId, registrationId, accreditationId, prnId, localise, session }
 ) {
   // Fetch PRN and registration data from backend
-  const [{ organisationData, registration, accreditation }, prn] =
-    await Promise.all([
-      fetchRegistrationAndAccreditation(
-        organisationId,
-        registrationId,
-        session.idToken
-      ),
-      fetchPackagingRecyclingNote(
-        organisationId,
-        registrationId,
-        accreditationId,
-        prnId,
-        session.idToken
-      )
-    ])
+  const [{ registration, accreditation }, prn] = await Promise.all([
+    fetchRegistrationAndAccreditation(
+      organisationId,
+      registrationId,
+      session.idToken
+    ),
+    fetchPackagingRecyclingNote(
+      organisationId,
+      registrationId,
+      accreditationId,
+      prnId,
+      session.idToken
+    )
+  ])
 
   if (!registration) {
     throw Boom.notFound('Registration not found')
@@ -280,7 +279,6 @@ async function handleExistingView(
 
   const prnDetailRows = buildExistingPrnDetailRows({
     prn,
-    organisationData,
     localise,
     noteType,
     statusConfig,
@@ -375,11 +373,11 @@ function buildExistingPrnViewData({
  * Builds the PRN/PERN details rows for a draft PRN (creation flow)
  * @param {object} params
  * @param {object} params.prnDraft - Draft PRN data from session
- * @param {object} params.organisationData - Organisation data
  * @param {(key: string) => string} params.localise - Translation function
+ * @param {object} params.organisationData - Organisation data
  * @returns {Array} Summary list rows
  */
-function buildDraftPrnDetailRows({ prnDraft, organisationData, localise }) {
+function buildDraftPrnDetailRows({ prnDraft, localise, organisationData }) {
   return [
     {
       key: { text: localise('lprns:issuedToLabel') },
@@ -407,11 +405,7 @@ function buildDraftPrnDetailRows({ prnDraft, organisationData, localise }) {
     },
     {
       key: { text: localise('lprns:issuerLabel') },
-      value: {
-        text:
-          organisationData.companyDetails?.name ||
-          localise('lprns:notAvailable')
-      }
+      value: { text: organisationData.companyDetails?.name || '' }
     },
     {
       key: { text: localise('lprns:issuedDateLabel') },
@@ -437,7 +431,6 @@ function buildDraftPrnDetailRows({ prnDraft, organisationData, localise }) {
  */
 function buildExistingPrnDetailRows({
   prn,
-  organisationData,
   localise,
   noteType,
   statusConfig,
@@ -457,7 +450,7 @@ function buildExistingPrnDetailRows({
 
   rows.push(
     ...buildPrnCoreRows(prn, localise, recipientDisplayName),
-    ...buildPrnAuthorisationRows(prn, organisationData, localise)
+    ...buildPrnAuthorisationRows(prn, localise)
   )
 
   return rows
