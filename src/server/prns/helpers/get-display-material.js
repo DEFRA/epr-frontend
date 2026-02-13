77bd80a11f26f15c43e01a9c684921641ffa8e6b
Philip Morris <philip@Philips-MacBook-Air.local>
2026-02-13 12:11:18 +0000

    remove getPrnDisplayMaterial

--- src/server/prns/helpers/get-display-material.js
+++
@@ -1,5 +0,0 @@
-import { getDisplayMaterial } from '#server/common/helpers/materials/get-display-material.js'
-
-export function getPrnDisplayMaterial(registration) {
-  return getDisplayMaterial(registration)
-}
