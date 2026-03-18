0e85150ed438993c7a304cc22ca4c2b66a5fc4a4
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-03-18 16:37:24 +0000

    sonarqube strikes again

--- src/server/summary-log/controller.js
+++ src/server/summary-log/controller.js
@@ -4,9 +4,9 @@ import {
   getDisplayCodeFromErrorCode,
   TECHNICAL_ERROR_DISPLAY_CODE
 } from '#server/common/constants/validation-codes.js'
+import { fetchRegistrationAndAccreditation } from '#server/common/helpers/organisations/fetch-registration-and-accreditation.js'
 import { fetchSummaryLogStatus } from '#server/common/helpers/upload/fetch-summary-log-status.js'
 import { initiateSummaryLogUpload } from '#server/common/helpers/upload/initiate-summary-log-upload.js'
-import { fetchRegistrationAndAccreditation } from '#server/common/helpers/organisations/fetch-registration-and-accreditation.js'
 import { fetchWasteBalances } from '#server/common/helpers/waste-balance/fetch-waste-balances.js'
 
 /** Waste record section number to display in UI copy, mapped by processing type */
@@ -434,7 +434,9 @@ const getWasteBalanceData = async (
 
     return { wasteBalance: balance.availableAmount }
   } catch (error) {
-    if (error.isBoom) throw error
+    if (error.isBoom) {
+      throw error
+    }
     logger.error({ err: error }, 'Failed to fetch waste balance data')
     return {}
   }
