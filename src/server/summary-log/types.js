4021c17af642ef9550f06a57a0e3c88f6a62fc67
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-03-20 16:57:40 +0000

    add missing or incorrect types

---
+++ src/server/summary-log/types.js
@@ -0,0 +1,63 @@
+/**
+ * @typedef {{
+ *   included: LoadRows,
+ *   excluded: LoadRows,
+ *   total: number
+ * }} LoadCategoryViewModel
+ */
+
+/**
+ * @typedef {{
+ *   errorCode: string,
+ *   location?: { header?: string }
+ * }} ValidationFailure
+ */
+
+/**
+ * @typedef {{
+ *   failures: ValidationFailure[]
+ * }} ValidationResponse
+ */
+
+/**
+ * @typedef {{
+ *   count: number,
+ *   rowIds: string[]
+ * }} LoadRows
+ */
+
+/**
+ * @typedef {{
+ *   added: LoadCategoryViewModel,
+ *   adjusted: LoadCategoryViewModel
+ * }} LoadsViewModel
+ */
+
+/**
+ * @typedef {{
+ *   valid: LoadRows,
+ *   invalid: LoadRows,
+ *   included: LoadRows,
+ *   excluded: LoadRows
+ * }} RawLoadCategory
+ */
+
+/**
+ * @typedef {{
+ *   added: RawLoadCategory,
+ *   adjusted: RawLoadCategory
+ *   unchanged: RawLoadCategory,
+ * }} RawLoads
+ */
+
+/**
+ * @typedef {{
+ *   accreditationNumber?: string,
+ *   loads?: RawLoads,
+ *   processingType?: string
+ *   status: string,
+ *   validation?: ValidationResponse,
+ * }} SummaryLogStatusResponse
+ */
+
+export {} // Required to make this file a module for JSDoc @import
