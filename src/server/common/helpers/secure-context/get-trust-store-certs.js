a2bbc134d4b5bd4af01da48053578896207bb603
David Lusby <loveboat@users.noreply.github.com>
2026-07-07 10:25:57 +0100

    PAE-1709: fix sonarqube warnings in frontend (#920)

--- src/server/common/helpers/secure-context/get-trust-store-certs.js
+++ src/server/common/helpers/secure-context/get-trust-store-certs.js
@@ -5,10 +5,8 @@
  */
 export function getTrustStoreCerts(envs) {
   return Object.entries(envs)
-    .map(([key, value]) => key.startsWith('TRUSTSTORE_') && value)
-    .filter(
-      /** @returns {envValue is string} */
-      (envValue) => Boolean(envValue)
+    .flatMap(([key, value]) =>
+      key.startsWith('TRUSTSTORE_') && value ? [value] : []
     )
     .map((envValue) => Buffer.from(envValue, 'base64').toString().trim())
 }
