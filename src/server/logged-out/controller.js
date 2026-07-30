5f87afa63cadf21f6d610913c4bc77e28838f7f3
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2026-07-30 10:30:05 +0100

    PAE-1798 consistently refer to definitions in paths
    - remove duplicate declarations of application urls (related to auth)
    - differentiate application logout url and oidc provider logout url in auth/logout handler

--- src/server/logged-out/controller.js
+++ src/server/logged-out/controller.js
@@ -1,3 +1,5 @@
+import { paths } from '#server/paths.js'
+
 /**
  * Logged out confirmation controller
  * Displays confirmation page after user has been logged out
@@ -18,7 +20,8 @@ const controller = {
     const { t: localise } = request
 
     return h.view('logged-out/index', {
-      pageTitle: localise('logged-out:pageTitle')
+      pageTitle: localise('logged-out:pageTitle'),
+      signInAgainHref: request.localiseUrl(paths.auth.login)
     })
   }
 }
